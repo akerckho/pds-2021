c113d7c8200ba6e0c036ac2c27dce8e2e99f82da
akerckho <akerckho@ulb.be>
2021-04-28 20:42:00 +0200

    Fixed plotting

--- RL/plot_results.py
+++ RL/plot_results.py
@@ -1,19 +1,24 @@
 import csv 
 import matplotlib.pyplot as plt
+import numpy as np
 
-class Ploter(object):
+class Ploter():
     def __init__(self, learning_rate, gamma):
         self.lr = learning_rate
         self.gamma = gamma
 
-        path_test_raw = "Results/0.05_tilesVisited.csv"
-        path_test_mean = "Results/0.05_tilesVisitedAverage.csv"
+        path_test_raw = "Results/5%/0.05_tilesVisited.csv"
+        path_test_mean = "Results/5%/0.05_tilesVisitedAverage.csv"
         fig_name = ["test0.png", "test1.png", "test2.png", "test3.png", "test4.png"]
 
         raw_test = self.load_from_file(path_test_raw)
         mean_test = self.load_from_file(path_test_mean)
-        for i in range(1): # files contains 5 iterations 
-            self.plot_bar_and_mean(raw_test[i], mean_test[i], fig_name[i])
+        #for i in range(5): # files contains 5 iterations 
+        #    self.plot_bar_and_mean(raw_test[i], mean_test[i], fig_name[i])
+        
+        self.plot_mean_over_files(self.mean_over_files(mean_test), 5, "testMeanOverFiles.png")
+        
+        
 
     def plot_bar_and_mean(self, raw_lst, mean_lst, fig_name):
         x = [i for i in range(len(raw_lst))]
@@ -25,16 +30,34 @@ class Ploter(object):
         plt.xlabel("Epoque")
         plt.ylabel("Nombre de tiles parcourues")
         plt.savefig(fig_name)
+        plt.cla()
+        plt.clf()
+
+    def plot_mean_over_files(self, mean_over_files_lst, wall_rate, fig_name):
+        x = [i for i in range(len(mean_over_files_lst))]
+         
+        plt.axis((0,2000,0,280))
+        plt.yticks(np.arange(0, 280, step=20))
+        plt.plot(x, mean_over_files_lst, color="tab:red")
+        plt.title(f"Moyenne sur 5 entraînements.\n Learning rate = {self.lr}, gamma = {self.gamma}, wall rate = {wall_rate}%")
+        plt.xlabel("Epoque")
+        plt.ylabel("Nombre de tiles parcourues")
+        plt.savefig(fig_name)
+        plt.cla()
+        plt.clf()
+
 
     def mean_over_files(self, files_lst):
-        pass
+        data = np.array(files_lst)
+        return np.average(data, axis=0)
+ 
 
     def load_from_file(self, path):
         with open(path, newline='') as file:
             csv_reader = csv.reader(file)
             to_plot = []
             for row in csv_reader:
-                to_plot.append(row)
+                to_plot.append(list(map(float,row)))
 
         return to_plot
 
